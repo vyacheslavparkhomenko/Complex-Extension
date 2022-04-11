05af77806168fc370ef1a7a452ae56610e514c21
VParkhomenko <vyacheslav.parkhomenko@gmail.com>
2022-04-12 00:36:55 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ComplexExtension',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/vyacheslavparkhomenko/Complex-Extension.git',
+    license='NoLicense',
+    author='VVP',
+    author_email='vyacheslav.parkhomenko@gmail.com',
+    description='Extension for working with complex numeric'
+)
